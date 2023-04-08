d732b82f9812a73c79b04bf7a99a5dc8c938edf1
erosunica <lovemetal_vgf@hotmail.com>
2023-04-08 19:29:56 +0200

    Fix design flaw: Pic banks are offset by `PICS_FIX`

--- engine/gfx/load_pics.asm
+++ engine/gfx/load_pics.asm
@@ -121,7 +121,6 @@ GetFrontpicPointer:
 	call AddNTimes
 	ld a, d
 	call GetFarByte
-	call FixPicBank
 	push af
 	inc hl
 	ld a, d
@@ -223,7 +222,6 @@ GetMonBackpic:
 	add hl, bc
 	ld a, d
 	call GetFarByte
-	call FixPicBank
 	push af
 	inc hl
 	ld a, d
@@ -243,7 +241,7 @@ GetMonBackpic:
 	ldh [rSVBK], a
 	ret
 
-FixPicBank:
+FixPicBank: ;unused
 ; This is a thing for some reason.
 
 PICS_FIX EQU $36
@@ -296,7 +294,6 @@ Function511ec:
 	call AddNTimes
 	ld a, BANK(PokemonPicPointers)
 	call GetFarByte
-	call FixPicBank
 	push af
 	inc hl
 	ld a, BANK(PokemonPicPointers)
@@ -327,7 +324,6 @@ GetTrainerPic:
 	push de
 	ld a, BANK(TrainerPicPointers)
 	call GetFarByte
-	call FixPicBank
 	push af
 	inc hl
 	ld a, BANK(TrainerPicPointers)
