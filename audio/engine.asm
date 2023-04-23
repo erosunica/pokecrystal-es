erosunica/pokecrystal-es

commit eae6f3468b06bd99202a78944292a583a5438b39

Optimize: a = X +/- carry

audio/engine.asm
@@ -729,8 +729,8 @@ LoadNote:
 	ld a, e
 	sub [hl]
 	ld e, a
-	ld a, d
-	sbc 0
+	sbc d
+	add e
 	ld d, a
 	ld hl, CHANNEL_PITCH_SLIDE_TARGET + 1
 	add hl, bc
@@ -751,8 +751,8 @@ LoadNote:
 	ld a, [hl]
 	sub e
 	ld e, a
-	ld a, d
-	sbc 0
+	sbc d
+	add e
 	ld d, a
 	; ????
 	ld hl, CHANNEL_PITCH_SLIDE_TARGET + 1
@@ -778,8 +778,8 @@ LoadNote:
 	ld a, e
 	sub [hl]
 	ld e, a
-	ld a, d
-	sbc 0
+	sbc d
+	add e
 	ld d, a
 	ld hl, CHANNEL_PITCH_SLIDE_TARGET + 1
 	add hl, bc
@@ -1002,8 +1002,8 @@ ApplyPitchSlide:
 	ld e, [hl]
 	sub e
 	ld e, a
-	ld a, d
-	sbc 0
+	sbc d
+	add e
 	ld d, a
 	; [Channel*Field25] *= 2
 	; if rollover: Frequency -= 1

commit 688e94f21e24d8b5f1f81a2c6db090677dcc18d0

Fix audio bugs
Introduced in eae6f34

audio/engine.asm
@@ -730,8 +730,8 @@ LoadNote:
 	ld a, e
 	sub [hl]
 	ld e, a
-	sbc d
-	add e
+	sbc a
+	add d
 	ld d, a
 	ld hl, CHANNEL_PITCH_SLIDE_TARGET + 1
 	add hl, bc
@@ -752,8 +752,8 @@ LoadNote:
 	ld a, [hl]
 	sub e
 	ld e, a
-	sbc d
-	add e
+	sbc a
+	add d
 	ld d, a
 	; ????
 	ld hl, CHANNEL_PITCH_SLIDE_TARGET + 1
@@ -777,8 +777,8 @@ LoadNote:
 	add hl, bc
 	sub [hl]
 	ld e, a
-	sbc d
-	add e
+	sbc a
+	add d
 	ld d, a
 	ld hl, CHANNEL_PITCH_SLIDE_TARGET + 1
 	add hl, bc
@@ -998,8 +998,8 @@ ApplyPitchSlide:
 	ld e, [hl]
 	sub e
 	ld e, a
-	sbc d
-	add e
+	sbc a
+	add d
 	ld d, a
 	; [Channel*Field25] *= 2
 	; if rollover: Frequency -= 1